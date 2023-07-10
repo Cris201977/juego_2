57d8e31cecad579653acb404974d4780377822a5
Cris201977 <cris.iban2016@gmail.com>
2023-07-10 08:54:21 -0300

    version10.07

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
-let play = document.querySelector("#play");
-play.addEventListener("click", jugar);
+
 let timer = 0;
 let contador = 0;
 let max = 3;
@@ -8,10 +7,10 @@ let max = 3;
 iniciar();
 
 
-
-
 function iniciar(){
-    timer += 10;
+    let play = document.querySelector("#play");
+    play.addEventListener("click", jugar);
+    timer += 20;
     document.getElementById('cuenta').innerHTML = "Reloj: " + timer;
     $('.card').load("./preguntas/inicio.txt");
 
@@ -19,25 +18,9 @@ function iniciar(){
 function jugar(){
     contador = elegirPregunta();
     $('.card').load("./preguntas/cargando.txt");
-
     setTimeout(() => {
-        $('.card').load("./preguntas/p_"+ contador  +".txt");
-        $('.resp_1').load("./respuestas/r_" + contador + "/r_1.txt");
-        $('.resp_2').load("./respuestas/r_" + contador + "/r_2.txt");
-        $('.resp_3').load("./respuestas/r_" + contador + "/r_3.txt");
-
-        let ganador = document.querySelector(".v");
-        ganador.addEventListener("click", ganar);
-        let perdedor = document.querySelector(".f");
-        perdedor.addEventListener("click", perder);
-
-        function ganar() {
-            alert("GANASTE");
-        }
-        function perder() {
-            alert("PERDISTE");
-        }
-        cuentaRegresiva() ;
+       cuentaRegresiva();
+       darOpciones();
     }, "3000");
 }
 
@@ -58,9 +41,44 @@ function cuentaRegresiva() {
         setTimeout("cuentaRegresiva()", 1000);
     }
 }
+
 function limpiarRespuestas(){
     $('.resp_1').load("./respuestas/r.txt");
     $('.resp_2').load("./respuestas/r.txt");
     $('.resp_3').load("./respuestas/r.txt");
+}
+
+function darOpciones(){
+    if(timer !=0){
+    $('.card').load("./preguntas/p_" + contador + ".txt");
+    $('.resp_1').load("./respuestas/r_" + contador + "/r_1.txt");
+    $('.resp_2').load("./respuestas/r_" + contador + "/r_2.txt");
+    $('.resp_3').load("./respuestas/r_" + contador + "/r_3.txt");
+    responder();
+
+    }
+}
+function responder(){
+    let juego = document.querySelector("#juego");
+    juego.addEventListener("click", r);
+
+    function r() {
+        alert("HOOOOO");
+    }
+    let ganador = document.querySelector("#v");
+    ganador.addEventListener("click", ganar);
+    let perdedor1 = document.querySelector("#f");
+    let perdedor2 = document.querySelector("#f_");
+    perdedor1.addEventListener("click", perder);
+    perdedor2.addEventListener("click", perder);
+
+    function ganar() {
+        alert("GANASTE");
+        timer == 0;
+    }
+    function perder() {
+        alert("PERDISTE");
+        timer == 0;
+    }
 }
 
